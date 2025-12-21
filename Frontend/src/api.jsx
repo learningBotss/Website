e43325c36090609d935c16d87cd936e3734a3b03
ArifHilmi <arifhilmi007@gmail.com>
2025-12-21 14:57:42 +0800

    api website

--- Frontend/src/api.jsx
+++ Frontend/src/api.jsx
@@ -1,19 +1,14 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api", 
+  baseURL: `${process.env.REACT_APP_BACKEND_URL}/api`,
 });
 
+//baseURL: `${process.env.REACT_APP_BACKEND_URL}/api`
+//baseURL: "http://localhost:8000/api"
+
 export const getQualificationQuiz = () => api.get("/qualification");
-export const postQualificationResult = async (answers) => {
-  return fetch("http://localhost:8000/api/qualification", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ answers }), 
-  }).then((res) => res.json());
-};
+export const postQualificationResult = (answers) => api.post("/qualification", { answers });
 
 export const getDisabilityQuestions = (name) => api.get(`/quiz/${name}`);
 export const postDisabilityResult = (name, answers) => api.post(`/quiz/${name}`, { answers });
